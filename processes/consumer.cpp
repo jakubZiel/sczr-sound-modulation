4d72d67d8123db05d039e7e365f8d754a7b90493
LWronska <laura.wronska12@gmail.com>
2021-05-04 19:21:33 +0200

    message queues test implementation

--- processes/consumer.cpp
+++ processes/consumer.cpp
@@ -10,6 +10,7 @@
 #include "utilities.h"
 
 #include <boost/interprocess/managed_shared_memory.hpp>
+#include <supervisor/supervisor.h>
 
 using namespace boost::interprocess;
 
@@ -17,9 +18,19 @@ void consumer(){
 
     //TODO binding to CPU
 
+    //get access to queues
+    message_queue modCons_mq(open_only, "modifierConsumer_mq");
+    message_queue consMod_mq(open_only, "consumerModifier_mq");
+
+    int messageBuffer[1];
+    message_queue::size_type recvd_size;
+    unsigned int priority;
+    while( !modCons_mq.try_receive(&messageBuffer[0], sizeof(int), recvd_size, priority));
+
+    std::cout << "nastepna probka do wyslania do karty: " << messageBuffer[0] << std::endl;
+
     //get access to shared memory
     managed_shared_memory shMemory(open_only, "SoundBufferMemory");
-
     int *sample = shMemory.find<int>("modifierConsumerBuffer").first;
  //   int mcbsize = shMemory.get_instance_length("producerModifierBuffer");
 
@@ -30,19 +41,19 @@ void consumer(){
     int consBuffer[BUFFSIZE];
 
     std::cout << "Consumer. Data from sample put into buffer: " << std::endl;
-    for (int j=0; j<MEETING; j++) {
+    for (int j=0; j<BUFFSIZE; j++) {
         consBuffer[j] = *(sample + j);
         std::cout << consBuffer[j] << " ";
     }
 
+    while (!consMod_mq.try_send(&messageBuffer[0], sizeof (int), 0));
+
     std::cout << std::endl;
+    std::cout << "ile mess w modCons? " << modCons_mq.get_num_msg() << std::endl;
+    std::cout << "ile mess w consMod? " << consMod_mq.get_num_msg() << std::endl;
 
     //TODO put changed samples to alsa buffer
 
 
-//    std::cout << "Consumer. Destroying sample: " << std::endl;
-//    managed_shm.destroy<int>("Sample");
-//    std::pair<int*, std::size_t> p = managed_shm.find<int>("Sample");
-//    std::cout << p.first << '\n';
 
 }
--- supervisor/supervisor.cpp
+++ supervisor/supervisor.cpp
@@ -5,14 +5,15 @@
 #define SH_MEMORY_SIZE 65536
 #define MQ_SIZE 200
 #include "supervisor.h"
+#include "processes/utilities.h"
 #include <boost/interprocess/containers/vector.hpp>
 #include <iostream>
 
 
 using namespace boost::interprocess;
 
-typedef allocator<int, managed_shared_memory::segment_manager>  shMemAllocator;
-typedef vector<int, shMemAllocator> sharedVector;
+//typedef allocator<int, managed_shared_memory::segment_manager>  shMemAllocator;
+//typedef vector<int, shMemAllocator> sharedVector;
 
 
 supervisor::supervisor() {
@@ -23,19 +24,23 @@ supervisor::supervisor() {
 supervisor::~supervisor() {
 
     delete producerModifier_mq;
+    delete modifierProducer_mq;
     delete modifierConsumer_mq;
+    delete consumerModifier_mq;
     delete shMemory;
 
     std::cout << "in destructor" << std::endl;
 
     message_queue::remove("producerModifier_mq");
+    message_queue::remove("modifierProducer_mq");
     message_queue::remove("modifierConsumer_mq");
+    message_queue::remove("consumerModifier_mq");
     shared_memory_object::remove("SoundBufferMemory");
 }
 
 void supervisor::init(){
-//    std::cout << "kolejki" << std::endl;
-   // init_queues();
+    std::cout << "kolejki" << std::endl;
+    init_queues();
 
     std::cout << "pamiec" << std::endl;
     init_shMemory();
@@ -50,27 +55,60 @@ void supervisor::init(){
 void supervisor::init_shMemory(){
 
     shMemory = new managed_shared_memory (open_or_create, "SoundBufferMemory", SH_MEMORY_SIZE);
+
 }
 
 void supervisor::init_buffers() {
-    shMemAllocator allocInstance(shMemory->get_segment_manager());
+//    shMemAllocator allocInstance(shMemory->get_segment_manager());
 
-    shMemory->construct<sharedVector>("producerModifierBuffer")(allocInstance);
-    shMemory->construct<sharedVector>("modifierConsumerBuffer")(allocInstance);
+//    shMemory->construct<sharedVector>("producerModifierBuffer")(allocInstance);
+//    shMemory->construct<sharedVector>("modifierConsumerBuffer")(allocInstance);
+    shMemory->construct<int>("producerModifierBuffer")[10](0);
+    shMemory->construct<int>("modifierConsumerBuffer")[10](0);
 }
 
 void supervisor::init_queues(){
 
+    //indexes of read samples
     producerModifier_mq = new message_queue(create_only
             , "producerModifier_mq"
-            , MQ_SIZE
+            , BUFFNUM
             , sizeof(int)
     );
 
+    //where can the read be put
+    modifierProducer_mq = new message_queue(create_only
+            , "modifierProducer_mq"
+            , BUFFNUM
+            , sizeof(int)
+    );
+
+    //fill modifierProducer_mq with initial values
+    int bufor[1];
+    for(int i=0; i<BUFFNUM; i++){
+        bufor[0]=i;
+        modifierProducer_mq->send(&bufor, sizeof (int), 0);
+    }
+
+    //indexes of modified samples
     modifierConsumer_mq = new message_queue(create_only
             , "modifierConsumer_mq"
             , MQ_SIZE
             , sizeof(int)
     );
+
+    //where can modified be put
+    consumerModifier_mq = new message_queue(create_only
+            , "consumerModifier_mq"
+            , MQ_SIZE
+            , sizeof(int)
+    );
+
+    //fill consumerModifier_mq with initial values
+    for(int i=0; i<BUFFNUM; i++){
+        bufor[0]=i;
+        consumerModifier_mq->send(&bufor, sizeof (int), 0);
+    }
 }
+
 
